a728d63a7a836ed30443e00e69fd03b4e2ae87aa
NV <ddd1020018@gmail.com>
2025-11-02 15:09:18 +0300

    select screen -> sleep screen

--- src/CutsceneChange.cs
+++ src/CutsceneChange.cs
@@ -12,19 +12,33 @@ public static class CutsceneChange
 
 	static void MenuSceneOnBuildScene(On.Menu.MenuScene.orig_BuildScene orig, MenuScene self)
 	{
-		if (self.menu is SlugcatSelectMenu
-		    && self.owner is SlugcatSelectMenu.SlugcatPageContinue pageContinue
+		if (self.menu is SleepAndDeathScreen screen
 		    && (Plugin.devMode 
-		    || pageContinue.saveGameData.shelterName is string shelterName
-		    && shelterName.Substring(0, shelterName.IndexOf("_", StringComparison.InvariantCulture)) == "O7"))
+		        || screen.saveState.denPosition is string shelterName
+		        && shelterName.Substring(0, shelterName.IndexOf("_", StringComparison.InvariantCulture)) == "O7"))
 		{
-			SlugcatStats.Name slugcatNumber = pageContinue.slugcatNumber;
+			SlugcatStats.Name slugcatNumber = screen.saveState.saveStateNumber;
 			MenuScene.SceneID replacementScene = new($"O7-SleepScreen-{slugcatNumber.value}");
 			if (SlugBase.Assets.CustomScene.Registry.TryGet(replacementScene, out _))
 			{
 				self.sceneID = replacementScene;
 			}
 		}
+		//leftovers of swapping slugcat select screen
+		
+		// if (self.menu is SlugcatSelectMenu
+		//     && self.owner is SlugcatSelectMenu.SlugcatPageContinue pageContinue
+		//     && (Plugin.devMode 
+		//     || pageContinue.saveGameData.shelterName is string shelterName
+		//     && shelterName.Substring(0, shelterName.IndexOf("_", StringComparison.InvariantCulture)) == "O7"))
+		// {
+		// 	SlugcatStats.Name slugcatNumber = pageContinue.slugcatNumber;
+		// 	MenuScene.SceneID replacementScene = new($"O7-SleepScreen-{slugcatNumber.value}");
+		// 	if (SlugBase.Assets.CustomScene.Registry.TryGet(replacementScene, out _))
+		// 	{
+		// 		self.sceneID = replacementScene;
+		// 	}
+		// }
 		orig(self);
 	}
 
