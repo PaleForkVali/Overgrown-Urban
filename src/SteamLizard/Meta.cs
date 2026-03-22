98bd08b8cf9fcfc930b80ab5687ec28650593594
NV <ddd1020018@gmail.com>
2026-03-22 14:49:46 +0300

    more cleanups

--- src/SteamLizard/Meta.cs
+++ src/SteamLizard/Meta.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using MoreSlugcats;
-using UnityEngine;
-using RWCustom;
-using Watcher;
-using Random = UnityEngine.Random;
-
 namespace OvergrownUrban.SteamLizard;
 
 public static class Meta
